ead66c66874e87ed63547fc62c29400dc133fccc
Elnur Qasımov <98845869+elnurumo@users.noreply.github.com>
2023-04-07 13:44:41 +0400

    Add files via upload

---
+++ Helper/Helper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Struct.Helper
+{
+    internal static class Helper
+    {
+
+        public static void CheckMail(this string email)
+        {
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            if (regex.IsMatch(email))
+            {
+                Console.WriteLine("Email is correct");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong Email is incorrect...");
+            }
+        }
+
+        public static void MatchCount(this string sentence, string word )
+        {
+            string[] arr = sentence.Split(' ');
+            int Count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].ToLower() == word.ToLower())
+                {
+                    Count++;
+                }
+            }
+            Console.WriteLine(Count);
+        }
+
+       
+        public static void CheckPolimor(this int number)
+        {
+            int copy = number;
+            int newnum = 0;
+            int mertebe;
+            while(number > 0)
+            {
+                mertebe = number % 10;
+                newnum = newnum *10 + mertebe;
+                number /= 10;
+            }
+            if(copy == newnum)
+            {
+                Console.WriteLine("Polemdromdur");
+            }else
+            {
+                Console.WriteLine("Polemdrom deyil");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Struct.Helper;
+
+namespace Struct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = "elnurqasumov592gmail.com";
+            email.CheckMail();
+            string sentece = "Salam Kamran necesen Kamran";
+            sentece.MatchCount("Kamran");
+
+            int num = 292;
+            num.CheckPolimor();
+        }
+    }
+}
